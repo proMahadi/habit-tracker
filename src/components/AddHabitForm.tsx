15d745f95ffc63f8c66d826952ac4bc6ae3774b0
proMahadi <professionalmahadi@gmail.com>
2024-12-13 20:27:42 +0600

    adding habit to redux

--- src/components/AddHabitForm.tsx
+++ src/components/AddHabitForm.tsx
@@ -1,16 +1,72 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Input } from "./ui/Input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  // SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/Select";
+import { Button } from "./ui/Button";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { addHabit } from "@/redux/app/habit/habitSlice";
 
+const AddHabitForm: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [frequency, setFrequency] = useState<"daily" | "weekly">("daily");
 
-const AddHabitForm:React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
 
-    const [name,setName]=useState<string>("");
-    const [frequency,setFrequency]=useState<"daily" | "weekly">("daily")
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (name.trim()) {
+      dispatch(
+        addHabit({
+          name,
+          frequency,
+        })
+      );
+      setName("");
+      setFrequency("daily");
+    }
+  };
   return (
-    <div className="max-w-[500px]">
-        <Input type="text" placeholder="enter habit name" className="capitalize"/>
-    </div>
-  )
-}
+    <form className="max-w-[500px] mx-auto" onSubmit={handleSubmit}>
+      <Input
+        value={name}
+        type="text"
+        placeholder="enter habit name"
+        className="capitalize border-black outline-none "
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div className="my-8">
+        <label htmlFor="frequency" className="font-medium text-2xl">
+          Frequency
+        </label>
+        <Select
+          value={frequency}
+          onValueChange={(value) => setFrequency(value as "daily" | "weekly")}
+        >
+          <SelectTrigger className="focus:outline-none mt-6 border-black">
+            <SelectValue placeholder="Select Frequency" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="daily">Daily</SelectItem>
+              <SelectItem value="weekly">Weekly</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
 
-export default AddHabitForm
+        <Button type="submit" className="w-full mt-4 hover:bg-blue-500 ">
+          Add Habit
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddHabitForm;
--- src/redux/app/habit/habitSlice.ts
+++ src/redux/app/habit/habitSlice.ts
@@ -1,29 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface Habit {
-    id:string;
-    name:string;
-    frequency:"daily" | "weekly";
-    completedDates:string[];
-    createdAt:string;
+  id: string;
+  name: string;
+  frequency: "daily" | "weekly";
+  completedDates: string[];
+  createdAt: string;
 }
 
 interface HabitState {
-    habits: Habit[];
+  habits: Habit[];
 }
 
-const initialState : HabitState = {
-    habits : [],
+const initialState: HabitState = {
+  habits: [],
 };
 
 const habitSlice = createSlice({
-    name:"habits",
-    initialState,
-    reducers:{
-        addHabit: ()=>{},
+  name: "habits",
+  initialState,
+  reducers: {
+    addHabit: (
+      state,
+      action: PayloadAction<{ name: string; frequency: "daily" | "weekly" }>
+    ) => {
+      const newHabit:Habit = {
+        id: Date.now().toString(),
+        name: action.payload.name,
+        frequency: action.payload.frequency,
+        completedDates: [],
+        createdAt: new Date().toISOString(),
+      };
+
+      state.habits.push(newHabit);
     },
+  },
 });
 
-
-export const {addHabit} = habitSlice.actions;
-export default habitSlice.reducer
+export const { addHabit } = habitSlice.actions;
+export default habitSlice.reducer;
